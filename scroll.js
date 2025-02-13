1e8c16407ca898f972a6cc0d1f597c631a74eb7b
ounjuu <koj1555@naver.com>
2025-02-13 13:40:41 +0900

    Feat: readme update

--- scroll.js
+++ scroll.js
@@ -9,21 +9,22 @@ window.addEventListener("scroll", () => {
   document.querySelector(".libox2").style.opacity = y2;
   document.querySelector(".libox3").style.opacity = y3;
 
-  // 두번째 스티키들
+  // // 두번째 스티키들
 
-  let y4 = -0.00114 * scrollHeight + 5.048;
-  let y5 = -0.0016 * scrollHeight + 7.792;
-  let y6 = -0.0016 * scrollHeight + 8.592;
-  let small1 = -0.0002857 * scrollHeight + 2.012;
-  let small2 = -0.0004 * scrollHeight + 2.698;
-  let small3 = -0.0004 * scrollHeight + 2.898;
-  document.querySelector(".appendimg1").style.opacity = y4;
-  document.querySelector(".appendimg2").style.opacity = y5;
-  document.querySelector(".appendimg3").style.opacity = y6;
-  document.querySelector(".appendimg1").style.scale = small1;
-  document.querySelector(".appendimg2").style.scale = small2;
-  document.querySelector(".appendimg3").style.scale = small3;
+  // let y4 = -0.00114 * scrollHeight + 5.048;
+  // let y5 = -0.0016 * scrollHeight + 7.792;
+  // let y6 = -0.0016 * scrollHeight + 8.592;
+  // let small1 = -0.0002857 * scrollHeight + 2.012;
+  // let small2 = -0.0004 * scrollHeight + 2.698;
+  // let small3 = -0.0004 * scrollHeight + 2.898;
+  // document.querySelector(".appendimg1").style.opacity = y4;
+  // document.querySelector(".appendimg2").style.opacity = y5;
+  // document.querySelector(".appendimg3").style.opacity = y6;
+  // document.querySelector(".appendimg1").style.scale = small1;
+  // document.querySelector(".appendimg2").style.scale = small2;
+  // document.querySelector(".appendimg3").style.scale = small3;
 });
+// y = a * 높이 * b
 // let y = (-1 / 450) * scrollHeight + 23 / 9;
 // let small = (-1 / 4500) * scrollHeight + 52 / 45;
 // document.querySelectorAll(".card-box")[0].style.opacity = y;
